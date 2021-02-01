3e3f7c43728d7bbf9de0ebc9b2ad2fe62c6fe27b
federiconova <federico.nova@stfc.ac.uk>
2021-02-01 16:49:35 +0000

    remove qqq

--- src/WCSimConstructCylinder.cc
+++ src/WCSimConstructCylinder.cc
@@ -153,8 +153,6 @@ G4LogicalVolume* WCSimDetectorConstruction::ConstructCylinder()
 				     0.*deg,
 				     360.*deg);
 
-  //  std::clog << " qqqqqqqqqqqqqqqqqqqq " << " WCRadius " << WCRadius << " WCBarrel radius " << WCRadius+1.*m << " half height "  << .5*WCLength << std::endl;
-  
   G4LogicalVolume* logicWCBarrel = 
     new G4LogicalVolume(solidWCBarrel,
 			G4Material::GetMaterial(water),
@@ -1031,9 +1029,6 @@ If used here, uncomment the SetVisAttributes(WClogic) line, and comment out the 
                                                  -barrelODCellWidth/2.+(i+0.5)*horizontalODSpacing+((G4int)(std::pow(-1,j))*(G4int)(WCODPMTShift)/2),
                                                  -(barrelCellHeight * (barrelODCellWidth/barrelCellWidth))/2.+(j+0.5)*verticalODSpacing);
 
-		//		std::cout << " qqqqqqqqqqqqqqqqqqqqqqqq barrel i " << i << " of " << WCPMTODperCellHorizontal << " j " << j << " of " << WCPMTODperCellVertical << " Container (" << Container.x() << ", " << Container.y()
-		//				  << ", " << Container.z() << ") " << std::endl;
-
         G4VPhysicalVolume* physiWCBarrelWLSPlate =
             new G4PVPlacement(WCPMTODRotation,           // its rotation
                               Container,
@@ -1164,9 +1159,6 @@ If used here, uncomment the SetVisAttributes(WClogic) line, and comment out the 
 
         if (((sqrt(xoffset*xoffset + yoffset*yoffset) + WCPMTODRadius) < WCODCapEdgeLimit) ) {
 
-
-		  //		  std::cout << " qqqqqqqqqqqqqqqqqqqqqqqq cap i " << i << " of " << CapNCell << " j " << j << " of " << CapNCell << " Container (" << topWLSpos.x() << ", " << topWLSpos.y()
-		  //				  << ", " << topWLSpos.z() << ") " << std::endl;
 
 			G4VPhysicalVolume* physiTopCapWLSPlate =
 					new G4PVPlacement(0,                   // its rotation
